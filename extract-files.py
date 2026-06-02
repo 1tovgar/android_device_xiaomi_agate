74220a47e7f50144cb9f6e03834a996a8544a995
1tovgar <tovgar@proton.me>
2026-06-02 13:05:28 +0500

    agate: proprietary: Pass ELF checks
    Signed-off-by: 1tovgar <tovgar@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -33,6 +33,13 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib/hw/audio.primary.mt6893.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautilsv2.so')
         .add_needed('libstagefright_foundation-v33.so'),
+
+    ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek',
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
+        .add_needed('libcodec2_hidl@1.0.so')
+        .add_needed('libbase_shim.so')
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
     
     ('vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/libmtkcam_stdutils.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
@@ -56,10 +63,6 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib/hw/vendor.mediatek.hardware.bluetooth.audio@2.2-impl.so', 'vendor/lib64/hw/vendor.mediatek.hardware.bluetooth.audio@2.2-impl.so'): blob_fixup()
         .replace_needed('vendor.mediatek.hardware.audio@6.1.so', 'vendor.mediatek.hardware.audio@7.1.so'),
 
-    'system_ext/lib64/libimsma.so': blob_fixup()
-        .replace_needed('libsink.so', 'libsink-mtk.so')
-        .add_needed('libshim_sink.so'),
-
     ('vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron', 'vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
         .add_needed('libbase_shim.so'),
 
@@ -69,7 +72,42 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
-        
+
+    'vendor/lib64/hw/hwcomposer.mt6893.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .binary_regex_replace(
+            b'OnScreenFingerprintPressedIcon',
+            b'SurfaceView[UdfpsControllerOve'
+    ),
+
+    'vendor/bin/mnld': blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'libsensorndkbridge-v30.so'),
+
+    ('vendor/lib64/libaalservice.so', 'vendor/lib/libaalservice.so'): blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
+
+    'vendor/lib64/mt6893/libmnl.so': blob_fixup()
+        .add_needed('libcutils.so'),
+
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+
+    ('vendor/lib/libthha.so', 'vendor/lib/libvcodec_oal.so'): blob_fixup()
+        .clear_symbol_version('__aeabi_memcpy')
+        .clear_symbol_version('__aeabi_memset')
+        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
+    
+    ('vendor/lib/libteei_daemon_vfs.so', 'vendor/lib64/libteei_daemon_vfs.so'): blob_fixup()
+        .add_needed('liblog.so'),
+
+
+    ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalAILDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    
 }  # fmt: skip
 
 module = ExtractUtilsModule(
